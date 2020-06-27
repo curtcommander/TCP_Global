8f37a63a9898772d1e36a1d06894ff1f6750c2d0
curtcommander <curtcommander@gmail.com>
2020-06-26 20:57:43 -0400

    added downloading files

---
+++ download.js
@@ -0,0 +1,59 @@
+#!/usr/bin/env node
+
+'use strict';
+
+const fs = require('fs');
+const {_checkDrive} = require('./drive');
+const utils = require('./utils');
+const { getFiles } = require('./utils');
+
+downloadFile = _checkDrive(downloadFile);
+function downloadFile({fileId, fileName} = {}, drive) {
+    
+    return utils._collapseOptsToFileId(arguments[0])
+    .then((opts) => _getFileMetadata(opts.fileId))
+    .then((metadata) => _download(metadata))
+    
+    function _getFileMetadata(fileId) {
+        return getFiles({fileId, fields: 'name, id, mimeType'}, drive)
+        .then((result) => {
+            const responseData = result[0];
+            const metadata = {
+                fileId : responseData.id,
+                fileName : responseData.name, 
+                mimeType : responseData.mimeType
+            }
+            console.log('Retrieved metadata for', metadata.fileName)
+            return metadata;
+        })
+    }
+
+    function _download(metadata) {
+        // folder specified, throw error
+         if (metadata.mimeType === 'application/vnd.google-apps.folder') {
+             return console.error(new Error('A directory (rather than a file) was specified.'))
+         }
+        // download file
+        const dest = fs.createWriteStream(metadata.fileName);
+        drive.files.get({fileId: metadata.fileId, alt: 'media'}, {responseType: 'stream'},
+        (error, response) => {
+            if (error) return console.error(error);
+            response.data
+            .on('end', () => {
+                console.log('Downloaded', metadata.fileName);
+            })
+            .on('error', error => {
+                console.log('Error while downloading', metadata.fileName+':', error);
+            })
+            .pipe(dest);
+        })
+    }
+}
+
+function downloadFileId(fileId, drive) {
+    return downloadFile({fileId}, drive)
+}
+
+function downloadFileName(fileName, drive) {
+    return downloadFile({fileName}, drive)
+}
