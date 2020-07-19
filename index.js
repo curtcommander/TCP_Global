ad5e771049f8d1825e6292cde83ce21db6a644c8
curtcommander <curtcommander@gmail.com>
2020-07-19 12:50:55 -0400

    added docdash theme and updated docs for utils

--- index.js
+++ index.js
@@ -3,12 +3,13 @@
 
 const config = require('./config');
 const utils = require('./lib/utils');
-const download = require('./lib/download');
+const down = require('./lib/download');
 const create = require('./lib/create');
 const del = require('./lib/delete');
 
 /**
- * Base class for utils-gdrive
+ * Base class whose methods are utils-google-drive functions.
+ * An instance of this class is returned when requiring the package.
  */
 class utilsGDrive {
     constructor() {
@@ -18,30 +19,37 @@ class utilsGDrive {
         this.LOG        = config.LOG
     }
     
-    api                       = utils.api;
-    listFiles                 = utils.listFiles;
-    getFiles                  = utils.getFiles;
-    getFileId                 = utils.getFileId;
-    getFileName               = utils.getFileName; 
-    listChildren              = utils.listChildren;
-    listChildrenFileId        = utils.listChildrenFileId; 
-    listChildrenFileName      = utils.listChildrenFileName;
+    api                     = utils.api;
+    listFiles               = utils.listFiles;
+    getFiles                = utils.getFiles;
+    getFileId               = utils.getFileId;
+    getFileIdPId            = utils.getFileIdPId;
+    getFileIdPName          = utils.getFileIdPName;
+    getFileName             = utils.getFileName;
+    getMime                 = utils.getMime;
+    getMimeFId              = utils.getMimeFId;
+    getMimeFNamePId         = utils.getMimeFNamePId;
+    getMimeFNamePName       = utils.getMimeFNamePName;
+    listChildren            = utils.listChildren;
+    listChildrenFId         = utils.listChildrenFId; 
+    listChildrenFNamePId    = utils.listChildrenFNamePId;
+    listChildrenFNamePName  = utils.listChildrenFNamePName;
     
-    downloadFile              = download.downloadFile;
-    downloadFileId            = download.downloadFileId;
-    downloadFileName          = download.downloadFileName;
+    downloadFile            = down.downloadFile;
+    downloadFileId          = down.downloadFileId;
+    downloadFileName        = down.downloadFileName;
 
-    uploadFile                = create.uploadFile;
-    uploadFileParentId        = create.uploadFileParentId;
-    uploadFileParentName      = create.uploadFileParentName;
-    mkDir                     = create.mkDir;
-    mkDirParentId             = create.mkDirParentId;
-    mkDirParentName           = create.mkDirParentName;
+    uploadFile              = create.uploadFile;
+    uploadFilePId           = create.uploadFileParentId;
+    uploadFilePName         = create.uploadFileParentName;
+    mkDir                   = create.mkDir;
+    mkDirPId                = create.mkDirParentId;
+    mkDirPName              = create.mkDirParentName;
 
-    deleteFile                = del.deleteFile;
-    deleteFileId              = del.deleteFileId;
-    deleteFileNameParentId    = del.deleteFileNameParentId;
-    deleteFileNameParentName  = del.deleteFileNameParentName;
+    deleteFile              = del.deleteFile;
+    deleteFId               = del.deleteFileId;
+    deleteFNamePId          = del.deleteFileNameParentId;
+    deleteFNamePName        = del.deleteFileNameParentName;
 }
 
 module.exports = new utilsGDrive();
