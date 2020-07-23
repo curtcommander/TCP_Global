9a1034d965960ba4188ac4f59c4e83f9fa60433a
curtcommander <curtcommander@gmail.com>
2020-07-23 08:05:08 -0400

    initial round of unit testing

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 #!/usr/bin/env node
 'use strict';
 
+
 const config = require('./config');
 const utils = require('./lib/utils');
 const download = require('./lib/download');
 const upload = require('./lib/upload');
 const del = require('./lib/delete');
 const update = require('./lib/update');
+const drive = require('./lib/drive');
 
 /**
  * Base class whose methods are utils-google-drive functions.
@@ -20,7 +22,9 @@ class utilsGDrive {
         this.LOG        = config.LOG
     }
     
-    api                     = utils.api;
+    api                     = drive._addDrive(utils.api, this);
+    _resolveParamsId        = utils._resolveParamsId;
+    _resolveFId             = utils._resolveFId;
     listFiles               = utils.listFiles;
     getFiles                = utils.getFiles;
     updateFiles             = utils.updateFiles;
@@ -28,10 +32,17 @@ class utilsGDrive {
     getFileName             = utils.getFileName;
     getMime                 = utils.getMime;
     listChildren            = utils.listChildren;
-    download                = download.download;
+
+    download                = drive._addDrive(download.download, this);
+    _downloadFile           = download._downloadFile;
+
+    _checkExistsDrive       = upload._checkExistsDrive;
     mkDir                   = upload.mkDir;
     upload                  = upload.upload;
+    _uploadFile             = upload._uploadFile;
+
     del                     = del.del;
+
     rename                  = update.rename;
     mv                      = update.mv;
 }
