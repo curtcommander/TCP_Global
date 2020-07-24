161b6ed196728d0e08d585f054b1211d774047a8
curtcommander <curtcommander@gmail.com>
2020-07-24 13:04:27 -0400

    better error handling

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 #!/usr/bin/env node
 'use strict';
 
-
-const config = require('./config');
 const utils = require('./lib/utils');
 const download = require('./lib/download');
 const upload = require('./lib/upload');
@@ -10,40 +8,42 @@ const del = require('./lib/delete');
 const update = require('./lib/update');
 const drive = require('./lib/drive');
 
+class UtilsGDriveError extends Error {
+    constructor(args){
+        super(args);
+        this.name = "UtilsGDriveError"
+    }
+}
+
 /**
  * Base class whose methods are utils-google-drive functions.
  * An instance of this class is returned when requiring the package.
  */
-class utilsGDrive {
-    constructor() {
-        this.SCOPES     = config.SCOPES,
-        this.TOKEN_PATH = config.TOKEN_PATH, 
-        this.CREDS_PATH = config.CREDS_PATH,
-        this.LOG        = config.LOG
-    }
-    
-    api                     = drive._addDrive(utils.api, this);
-    _resolveParamsId        = utils._resolveParamsId;
-    _resolveFId             = utils._resolveFId;
-    listFiles               = utils.listFiles;
-    getFiles                = utils.getFiles;
-    updateFiles             = utils.updateFiles;
-    getFileId               = utils.getFileId;
-    getFileName             = utils.getFileName;
-    getMime                 = utils.getMime;
-    listChildren            = utils.listChildren;
+class utilsGDrive {    
+    api                = drive._addDrive(utils.api, this);
+    _resolveParamsId   = utils._resolveParamsId;
+    _resolveFId        = utils._resolveFId;
+    listFiles          = utils.listFiles;
+    getFiles           = utils.getFiles;
+    updateFiles        = utils.updateFiles;
+    getFileId          = utils.getFileId;
+    getFileName        = utils.getFileName;
+    getMime            = utils.getMime;
+    listChildren       = utils.listChildren;
 
-    download                = drive._addDrive(download.download, this);
-    _downloadFile           = download._downloadFile;
-    _checkExistsDrive       = upload._checkExistsDrive;
-    mkDir                   = upload.mkDir;
-    upload                  = upload.upload;
-    _uploadFile             = upload._uploadFile;
+    download           = drive._addDrive(download.download, this);
+    _downloadFile      = download._downloadFile;
+    _checkExistsDrive  = upload._checkExistsDrive;
+    mkDir              = upload.mkDir;
+    upload             = upload.upload;
+    _uploadFile        = upload._uploadFile;
 
-    del                     = del.del;
+    del                = del.del;
 
-    rename                  = update.rename;
-    mv                      = update.mv;
+    rename             = update.rename;
+    mv                 = update.mv;
+
+    Error              = UtilsGDriveError;
 }
 
 module.exports = new utilsGDrive();
