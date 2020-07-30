52f5650af2e8c4623f439cc763ff4d3479f22aef
curtcommander <curtcommander@gmail.com>
2020-07-29 22:01:06 -0400

    test: added more tests for errors

---
+++ test/errors.js
@@ -0,0 +1,121 @@
+'use strict';
+
+const assert = require('assert').strict;
+const utilsGDrive = require('../../utils-google-drive');
+
+const fileIdTest = '1Xsvaf0S00x-Tcq-HtxvCuUHnEgVE6W4X';
+const parentIdTest = '19FWsjMbtZzfVnbUdDvGLdHKc3E3zXk0k';
+
+describe('errors', function() {
+  const consoleError = console.error;
+  let output;
+  beforeEach(function(done) {
+    output = '';
+    console.error = (msg) => {
+      output += msg + '\n';
+    };
+    done();
+  });
+
+  afterEach(function() {
+    console.error = consoleError;
+    if (this.currentTest.state === 'failed') {
+      console.error(output);
+    }
+  });
+
+  it('api(), handle error from response', function() {
+    assert.rejects(async () => {
+      await utilsGDrive.api('files', 'list', {q: 'test1234'});
+    });
+  });
+
+  it('getFiles(), file id not specified', function() {
+    assert.rejects(async function() {
+      await utilsGDrive.getFiles({shouldBeFileId: 'foo'});
+    }, utilsGDrive.Error);
+  });
+
+  it('updateFiles(), file id not specified', function() {
+    assert.rejects(async function() {
+      await utilsGDrive.updateFiles({shouldBeFileId: 'foo'});
+    }, utilsGDrive.Error);
+  });
+
+  it('getMime(), invalid identifier name', async function() {
+    assert.rejects(async () => {
+      await utilsGDrive.getMime({f: fileIdTest});
+    });
+  });
+
+  it('listChildren(), invalid identifier name', async function() {
+    assert.rejects(async () => {
+      await utilsGDrive.listChildren({f: fileIdTest}, 'files(id)');
+    });
+  });
+
+  it('download(), invalid identifier name', async function() {
+    assert.rejects(async () => {
+      await utilsGDrive.download({f: 'test'}, '.');
+    });
+  });
+
+  it('upload(), invalid identifier name', async function() {
+    assert.rejects(async () => {
+      await utilsGDrive.upload({
+        localPath: 'test',
+        parentIdentifiers: {f: 'test'},
+      });
+    });
+  });
+
+  it('makeFolder(), invalid identifier name', async function() {
+    assert.rejects(async () => {
+      await utilsGDrive.makeFolder({
+        folderName: 'test',
+        parentIdentifiers: {f: 'test'},
+      });
+    });
+  });
+
+  it('move(), invalid identifier name', async function() {
+    assert.rejects(async () => {
+      await utilsGDrive.move({f: 'test'});
+    });
+  });
+
+  it('rename(), invalid identifier name', async function() {
+    assert.rejects(async () => {
+      await utilsGDrive.rename({f: 'test'});
+    });
+  });
+
+  it('del(), invalid identifier name', async function() {
+    assert.rejects(async () => {
+      await utilsGDrive.del({f: fileIdTest});
+    });
+  });
+
+  it('_checkExistsDrive(), file/folder already exists', async function() {
+    const fileMetadata = {
+      name: 'Daily Logs',
+      mimeType: 'application/vnd.google-apps.folder',
+      parents: [parentIdTest],
+    };
+    assert.rejects(async () => {
+      await utilsGDrive._checkExistsDrive(fileMetadata);
+    }, utilsGDrive.Error);
+  });
+
+  it('_handleListFilesResponse(), multiple files found', async function() {
+    const e = await utilsGDrive._handleListFilesResponse(
+        [1, 2], 'test message');
+    assert(e instanceof utilsGDrive.Error);
+  });
+
+  it('_handleListFilesResponse(), no files found', async function() {
+    const e = await utilsGDrive._handleListFilesResponse([], 'test message');
+    assert(e instanceof utilsGDrive.Error);
+  });
+});
+
