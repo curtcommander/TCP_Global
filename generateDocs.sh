10c862c16e88d5e45a7942fdbd63eaba83caf298
curtcommander <curtcommander@gmail.com>
2020-09-02 17:10:51 -0400

    docs: fixed broken link to UtilsGDrive.html

--- generateDocs.sh
+++ generateDocs.sh
@@ -1,4 +1,6 @@
 jsdoc index.js lib README.md -d docs -c ./jsdoc.json
 sed -i 's/Global/Methods/g' docs/*.html
 sed -i 's/global/methods/g' docs/*.html
+mv docs/utilsGDrive.html UtilsGDrive.html
+mv UtilsGDrive.html docs/
 mv docs/global.html docs/methods.html
